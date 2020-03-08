d0705d4631e54954edf598a559d1f44d582270a6
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-03-08 17:06:13 +0100

    Add Contact Details Window

---
+++ DesktopContacts/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContacts
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindows.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        public ContactDetailsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
